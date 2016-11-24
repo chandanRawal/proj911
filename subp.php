66ebbc757de75455a242ed059b6b8de7de04325e
jerry90123 <shashwatmahar12@gmail.com>
2016-11-24 14:57:02 +0530

    if sub append url

--- subp.php
+++ subp.php
@@ -5,13 +5,12 @@
 		$n_name = $_SESSION['tid'];
 		if(($_FILES['file']['type']) == "application/pdf"){
 				if(move_uploaded_file($_FILES['file']['tmp_name'], $targetfolder)){
-					echo "the file ". basename($_FILES['file']['name'])." uploaded";
 					rename("pdfs/".basename($_FILES['file']['name']), "pdfs/".$n_name);
+					header("location:addpdf.php?sub=true");
 				}else{
 					//echo "error";
 				}
 			}else{
 				
-		}
-		
+		}		
 ?>
