ae189094f4bd50d188e1626bd5cc160d9d43f746
jerry90123 <shashwatmahar12@gmail.com>
2016-11-24 14:54:20 +0530

    only pdf will be sub

--- addpdf.php
+++ addpdf.php
@@ -37,28 +37,28 @@
 				<div id="content">
 				
 						<div class="row">
-							
+
 							<div class="col-lg-8"></div>
-								
 										<section class="panel">
 												<header class="panel-heading sm" data-color="theme-inverse">
 														<h2><strong>ADD PDF FOR YOUR TEAM :</strong>&nbsp&nbsp&nbsp<?php echo $tname; ?></h2>
 												</header>
 
 												<div class="panel-body">
-														<form class="form-horizontal" data-collabel="3" data-alignlabel="left" action="./subp.php" method="post" enctype="multipart/form-data">
+														<form class="form-horizontal" data-collabel="3" data-alignlabel="left" action="./subp.php" method="post" enctype="multipart/form-data" onsubmit="return extens()">
 																
 																<div class="form-group">
 																		<label class="control-label">Select your pdf</label>
-																		<input type="file" name="file" size="50" />
+																		<input type="file" name="file" size="50" id = "cool"/>
 																		<br />																
 																</div>
-																
+
 																</br>
 																<div class="form-group offset">
 																		<div>
 																				<input type="submit" class="btn btn-theme" name="sub" value="Upload">
 																				<button type="reset" class="btn">Reset</button>
+																				<p style="visibility: hidden; color: red" id = "mesg">
 																		</div>
 																</div>
 														</form>
@@ -73,10 +73,27 @@
 						<!-- //content > row-->
 						
 				</div>
-				<!-- //content-->
+	<script type="text/javascript">
+		function extens() {
+			var x = document.getElementById('cool').value;
+			var file_type = x.substr(x.lastIndexOf('.')).toLowerCase();
+			if(file_type == '.pdf'){
+				return true;
+			}
+			else{
+
+				var hid = document.getElementById("mesg");
+				hid.style.visibility = "visible";
+				hid.innerHTML = "Submitted file is not a pdf";
+				return false;
+			}
+
+		}
+	</script>				<!-- //content-->
 		
 				
 		</div>
+
 		<!-- //main-->
 		
 	<!--
